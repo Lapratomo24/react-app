9394d2cc2ec5f8f032d49d26c93c321136d8a81e
Lapratomo24 <lapratomo24@yahoo.com>
2022-09-20 17:34:09 +0000

    add usestate

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import SearchBar from './components/SearchBar';
+import UseStateWithArrays from './components/UseStateWithArrays';
 
 function App() {
 
   return (
     <div className="App">
-      <SearchBar />
+      <UseStateWithArrays />
     </div>
   );
 }
